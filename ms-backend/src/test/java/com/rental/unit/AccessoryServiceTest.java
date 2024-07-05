69ccecaf67fee57a8e9d825cd94f9319fa9666aa
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-05 18:59:31 -0300

    refact(UnitTests): AccessoryServiceTest refactored

--- ms-backend/src/test/java/com/rental/unit/AccessoryServiceTest.java
+++ ms-backend/src/test/java/com/rental/unit/AccessoryServiceTest.java
@@ -1,9 +1,14 @@
 package com.rental.unit;
 
+import static com.rental.mock.AccessoryMock.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.eq;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.rental.entity.Accessory;
 import com.rental.repository.AccessoryRepository;
 import com.rental.service.AccessoryService;
@@ -13,195 +18,108 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class AccessoryServiceTest {
 
-  @Autowired
+  @InjectMocks
   AccessoryService accessoryService;
 
-  @MockBean
+  @Mock
   AccessoryRepository accessoryRepository;
 
   @Test
   public void testAccessoryRetrievalById() throws AccessoryNotFoundException {
-
     // Arrange
-    UUID accessoryId = UUID.randomUUID();
+   when(accessoryRepository.findById(eq(ACCESSORY_WITH_ID_01.getId())))
+        .thenReturn(Optional.of(ACCESSORY_WITH_ID_01));
 
-    Accessory accessory = new Accessory();
-    accessory.setId(accessoryId);
-    accessory.setName("Baby Comfort");
-    accessory.setDailyRate(50.00);
-    accessory.setDescription("Baby comfort plus top");
-    accessory.setQuantity(10);
-
-    // Act
-    Mockito.when(accessoryRepository.findById(eq(accessoryId)))
-        .thenReturn(Optional.of(accessory));
-
-    Accessory getAccessory = accessoryService.getAccessoryById(accessoryId);
-
-    Mockito.verify(accessoryRepository).findById(accessoryId);
+   // Act
+   Accessory getAccessory = accessoryService.getAccessoryById(ACCESSORY_WITH_ID_01.getId());
 
     // Assert
-    assertEquals(getAccessory.getId(), accessory.getId());
-    assertEquals(getAccessory.getName(), accessory.getName());
-    assertEquals(getAccessory.getDailyRate(), accessory.getDailyRate());
-    assertEquals(getAccessory.getDescription(), accessory.getDescription());
-    assertEquals(getAccessory.getQuantity(), accessory.getQuantity());
+    assertThat(getAccessory).isEqualTo(ACCESSORY_WITH_ID_01);
   }
 
   @Test
   public void testGetAllAccessories() {
-
     // Arrange
-    UUID accessory1Id = UUID.randomUUID();
-    UUID accessory2Id = UUID.randomUUID();
-
-    Accessory accessory1 = new Accessory();
-    accessory1.setId(accessory1Id);
-    accessory1.setName("Baby Comfort");
-    accessory1.setDailyRate(50.00);
-    accessory1.setDescription("Baby comfort plus top");
-    accessory1.setQuantity(10);
-
-    Accessory accessory2 = new Accessory();
-    accessory2.setId(accessory2Id);
-    accessory2.setName("GPS");
-    accessory2.setDailyRate(100.00);
-    accessory2.setDescription("GPS top master noob");
-    accessory2.setQuantity(5);
-
-    List<Accessory> accessories = Arrays.asList(accessory1, accessory2);
-
+    List<Accessory> accessories = Arrays.asList(ACCESSORY_WITH_ID_01, ACCESSORY_WITH_ID_02);
     Page<Accessory> page = new PageImpl<>(accessories);
     Pageable pageable = PageRequest.of(0, 2);
 
     // Act
-    Mockito.when(accessoryRepository.findAll(pageable)).thenReturn(page);
-
+    when(accessoryRepository.findAll(pageable)).thenReturn(page);
     List<Accessory> getAllAccessories = accessoryService.getAllAccessories(0, 2);
-
-    Mockito.verify(accessoryRepository).findAll(pageable);
+    verify(accessoryRepository).findAll(pageable);
 
     // Assert
     assertEquals(2, getAllAccessories.size());
-    assertEquals(accessory1.getId(), accessories.get(0).getId());
-    assertEquals(accessory1.getName(), accessories.get(0).getName());
-    assertEquals(accessory1.getDailyRate(), accessories.get(0).getDailyRate());
-    assertEquals(accessory1.getDescription(), accessories.get(0).getDescription());
-    assertEquals(accessory1.getQuantity(), accessories.get(0).getQuantity());
-
-    assertEquals(accessory2.getId(), accessories.get(1).getId());
-    assertEquals(accessory2.getName(), accessories.get(1).getName());
-    assertEquals(accessory2.getDailyRate(), accessories.get(1).getDailyRate());
-    assertEquals(accessory2.getDescription(), accessories.get(1).getDescription());
-    assertEquals(accessory2.getQuantity(), accessories.get(1).getQuantity());
+    assertThat(getAllAccessories).isEqualTo(accessories);
   }
 
   @Test
   public void testCreateAccessory() {
-
     // Arrange
-    Accessory accessory = new Accessory();
-    accessory.setId(UUID.randomUUID());
-    accessory.setName("Baby Comfort");
-    accessory.setDailyRate(20.00);
-    accessory.setDescription("Baby comfort plus top");
-    accessory.setQuantity(5);
+    when(accessoryRepository.save(ACCESSORY_CREATION)).thenReturn(ACCESSORY_CREATION);
 
     // Act
-    Mockito.when(accessoryRepository.save(accessory)).thenReturn(accessory);
-
-    Accessory newAccessory = accessoryService.createAccessory(accessory);
-
-    Mockito.verify(accessoryRepository).save(accessory);
+    Accessory newAccessory = accessoryService.createAccessory(ACCESSORY_CREATION);
+    verify(accessoryRepository).save(ACCESSORY_CREATION);
 
     // Assert
-    assertEquals(newAccessory.getId(), accessory.getId());
-    assertEquals(newAccessory.getName(), accessory.getName());
-    assertEquals(newAccessory.getDailyRate(), accessory.getDailyRate());
-    assertEquals(newAccessory.getDescription(), accessory.getDescription());
-    assertEquals(newAccessory.getQuantity(), accessory.getQuantity());
+    assertThat(newAccessory).isEqualTo(ACCESSORY_CREATION);
   }
 
   @Test
   public void testUpdateAccessory() throws AccessoryNotFoundException {
 
     // Arrange
-    UUID id = UUID.randomUUID();
-    Accessory existingAccessory = new Accessory();
-    existingAccessory.setId(id);
-    existingAccessory.setName("Baby");
-    existingAccessory.setDailyRate(50.00);
-    existingAccessory.setDescription("Baby comfort");
-    existingAccessory.setQuantity(5);
+    when(accessoryRepository.findById(ACCESSORY_WITH_ID_01.getId()))
+            .thenReturn(Optional.of(ACCESSORY_WITH_ID_01));
 
-    Accessory updatedAccessory = new Accessory();
-    updatedAccessory.setName("Baby comfort plus");
-    updatedAccessory.setDailyRate(60.00);
-    existingAccessory.setDescription("Baby comfort plus pro");
-    existingAccessory.setQuantity(10);
-
-    Mockito.when(accessoryRepository.findById(id)).thenReturn(Optional.of(existingAccessory));
-    Mockito.when(accessoryRepository.save(existingAccessory)).thenReturn(existingAccessory);
+    when(accessoryRepository.save(ACCESSORY_WITH_ID_01))
+            .thenReturn(ACCESSORY_WITH_ID_01);
 
     // Act
-    Accessory result = accessoryService.updateAccessory(updatedAccessory, id);
+    Accessory result = accessoryService.updateAccessory(ACCESSORY_UPDATED, ACCESSORY_WITH_ID_01.getId());
 
     // Assert
-    assertEquals(updatedAccessory.getName(), result.getName());
-    assertEquals(updatedAccessory.getDailyRate(), result.getDailyRate());
-    assertEquals(updatedAccessory.getDescription(), result.getDescription());
-    assertEquals(updatedAccessory.getQuantity(), result.getQuantity());
+    assertThat(result).isEqualTo(ACCESSORY_WITH_ID_01);
   }
 
   @Test
   public void testUpdateAccessoryNotFoundException() {
-
     // Arrange
-    UUID id = UUID.randomUUID();
-    Accessory updatedAccessory = new Accessory();
-    updatedAccessory.setName("Baby comfort");
-    updatedAccessory.setDailyRate(60.00);
-    updatedAccessory.setDescription("Baby comfort");
-    updatedAccessory.setQuantity(5);
-
-    Mockito.when(accessoryRepository.findById(id)).thenReturn(Optional.empty());
+    when(accessoryRepository.findById(ACCESSORY_WITH_ID_01.getId()))
+            .thenReturn(Optional.empty());
 
     // Act & Assert
     assertThrows(AccessoryNotFoundException.class,
-        () -> accessoryService.updateAccessory(updatedAccessory, id));
+        () -> accessoryService.updateAccessory(ACCESSORY_UPDATED, ACCESSORY_WITH_ID_01.getId()));
   }
 
   @Test
   public void testDeleteAccessory() throws AccessoryNotFoundException {
     // Arrange
-    UUID id = UUID.randomUUID();
-    Accessory existingAccessory = new Accessory();
-    existingAccessory.setId(id);
-    existingAccessory.setName("Accessory");
-    existingAccessory.setDailyRate(50.00);
-    existingAccessory.setDescription("Accessory updated");
-    existingAccessory.setQuantity(5);
-
-    Mockito.when(accessoryRepository.findById(id)).thenReturn(Optional.of(existingAccessory));
+    when(accessoryRepository.findById(ACCESSORY_WITH_ID_01.getId()))
+            .thenReturn(Optional.of(ACCESSORY_WITH_ID_01));
 
     // Act
-    Accessory result = accessoryService.deleteAccessory(id);
+    Accessory result = accessoryService.deleteAccessory(ACCESSORY_WITH_ID_01.getId());
 
     // Assert
-    assertEquals(existingAccessory, result);
-    Mockito.verify(accessoryRepository).delete(existingAccessory);
+    assertEquals(ACCESSORY_WITH_ID_01, result);
+    verify(accessoryRepository).delete(ACCESSORY_WITH_ID_01);
   }
 
   @Test
@@ -209,7 +127,7 @@ public class AccessoryServiceTest {
     // Arrange
     UUID id = UUID.randomUUID();
 
-    Mockito.when(accessoryRepository.findById(id)).thenReturn(Optional.empty());
+    when(accessoryRepository.findById(id)).thenReturn(Optional.empty());
 
     // Act & Assert
     assertThrows(AccessoryNotFoundException.class,
