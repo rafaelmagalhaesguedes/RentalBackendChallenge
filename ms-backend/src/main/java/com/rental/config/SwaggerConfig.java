fe7d4b06dcd37979d87854944e0dc0979b858d95
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-10 21:12:51 -0300

    fix(ms-backend): SwaggerConfig

--- ms-backend/src/main/java/com/rental/config/SwaggerConfig.java
+++ ms-backend/src/main/java/com/rental/config/SwaggerConfig.java
@@ -2,9 +2,12 @@ package com.rental.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,6 +15,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(Collections.singletonList(
+                        new Server().url("https://ms-backend.up.railway.app").description("Principal server")
+                ))
                 .info(new Info()
                         .title("Rental Car System API")
                         .version("1.0")
