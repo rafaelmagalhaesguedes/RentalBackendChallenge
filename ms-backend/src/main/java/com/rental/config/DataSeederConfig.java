c59bb364e4224dd74a6b207920e9f8cd373bd11c
Rafael Guedes <rafaelmagalhaes1@live.com>
2024-07-13 13:23:49 -0300

    fix(ms-backend): seedGroups

--- ms-backend/src/main/java/com/rental/config/DataSeederConfig.java
+++ ms-backend/src/main/java/com/rental/config/DataSeederConfig.java
@@ -2,7 +2,6 @@ package com.rental.config;
 
 import com.rental.entity.Accessory;
 import com.rental.entity.Group;
-import com.rental.entity.Vehicle;
 import com.rental.repository.AccessoryRepository;
 import com.rental.repository.GroupRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -32,21 +31,13 @@ public class DataSeederConfig implements CommandLineRunner {
     private void seedGroups() {
         List<Group> groups = new ArrayList<>();
 
-        Group groupA = new Group(UUID.randomUUID(), "Grupo A", "Uno, Mobi, Kwid ou similar", 150.00);
-        Group groupB = new Group(UUID.randomUUID(), "Grupo B", "Argo, Gol, HB20 ou similar", 200.00);
-        Group groupC = new Group(UUID.randomUUID(), "Grupo C", "Chronos, Logan, HB20S ou similar", 250.00);
-        Group groupD = new Group(UUID.randomUUID(), "Grupo D", "Creta, Captur, T-Cross ou similar", 280.00);
-        Group groupE = new Group(UUID.randomUUID(), "Grupo E", "Renegade, Toro ou similar", 300.00);
-        Group groupF = new Group(UUID.randomUUID(), "Grupo F", "Doblò, Spin ou similar", 350.00);
-        Group groupG = new Group(UUID.randomUUID(), "Grupo G", "Hilux, S10, Ranger ou similar", 350.00);
-
-        groups.add(groupA);
-        groups.add(groupB);
-        groups.add(groupC);
-        groups.add(groupD);
-        groups.add(groupE);
-        groups.add(groupF);
-        groups.add(groupG);
+        groups.add(new Group(UUID.randomUUID(), "Grupo A", "Uno, Mobi, Kwid ou similar", 150.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo B", "Argo, Gol, HB20 ou similar", 200.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo C", "Chronos, Logan, HB20S ou similar", 250.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo D", "Creta, Captur, T-Cross ou similar", 280.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo E", "Renegade, Toro ou similar", 300.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo F", "Doblò, Spin ou similar", 350.00));
+        groups.add(new Group(UUID.randomUUID(), "Grupo G", "Hilux, S10, Ranger ou similar", 350.00));
 
         groupRepository.saveAll(groups);
     }
@@ -54,6 +45,10 @@ public class DataSeederConfig implements CommandLineRunner {
     private void seedAccessories() {
         List<Accessory> accessories = new ArrayList<>();
 
+        accessories.add(new Accessory(UUID.randomUUID(), "Proteção Básica", "Adicione a Proteção Básica do Carro, e viaje com a cobertura contra danos no casco (partes do carro) e roubo. Conte com cobrança limitada a um valor máximo preestabelecido nos casos de avarias (quaisquer danos), perda total, roubo e furto.", 1, 12.00));
+        accessories.add(new Accessory(UUID.randomUUID(), "Proteção Especial", "Essa proteção te deixa seguro com relação a danos aos vidros laterais, para-brisas, vidro traseiro, retrovisores, faróis, lanternas e/ou pneus do veículo. Assim, você tem isenção de custo pré-fixado de limite de danos para esses itens e não paga nada em caso de avarias.", 1, 20.00));
+        accessories.add(new Accessory(UUID.randomUUID(), "Limpeza Simples Garantida", "Com a Limpeza Garantida você não precisa nem se preocupar em lavar o carro antes de devolver. Você antecipa uma lavagem simples, que ficará por nossa conta.", 1, 30.00));
+        accessories.add(new Accessory(UUID.randomUUID(), "Limpeza Especial", "A lavagem especial é solicitada naquelas situações em que são necessários processos mais cuidadosos e demorados de higienização. Exemplos: se o carro for devolvido com areia ou pelos de animais, será cobrada o valor da lavagem especial, independente da quantidade do material.", 5, 150.00));
         accessories.add(new Accessory(UUID.randomUUID(), "GPS", "SmartPhone Samsumg com GPS integrado.", 5, 60.00));
         accessories.add(new Accessory(UUID.randomUUID(), "Bebê Conforto", "Cadeira para bebê até 5 anos de idade.", 10, 30.00));
         accessories.add(new Accessory(UUID.randomUUID(), "Assento de Elevação", "Assento de Elevação  para bebê até 5 anos de idade.", 10, 25.00));
